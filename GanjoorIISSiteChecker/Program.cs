3585f6b9a27558326c47f80faacff58fd659c780
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2023-12-08 21:39:45 +0330

    changing chatgpt generated code

--- GanjoorIISSiteChecker/Program.cs
+++ GanjoorIISSiteChecker/Program.cs
@@ -1,66 +1,36 @@
-﻿class Program
+﻿using Microsoft.Web.Administration;
+
+class Program
 {
     static void Main()
     {
-        string siteName = "api.ganjoor.net"; // Replace with your actual website name
-        string appPoolName = "api.ganjoor.net"; // Replace with your actual application pool name
+        string siteName = "api.ganjoor.net";
+        string appPoolName = "api.ganjoor.net";
 
-        if (IsSiteStopped(siteName))
+        using (ServerManager serverManager = new ServerManager())
         {
-            StartSite(siteName);
-            Console.WriteLine($"Website '{siteName}' has been started.");
-        }
-        else
-        {
-            Console.WriteLine($"Website '{siteName}' is already running.");
-        }
+            Site site = serverManager.Sites[siteName];
+            ApplicationPool appPool = serverManager.ApplicationPools[appPoolName];
 
-        if (IsAppPoolStopped(appPoolName))
-        {
-            StartAppPool(appPoolName);
-            Console.WriteLine($"Application pool '{appPoolName}' has been started.");
-        }
-        else
-        {
-            Console.WriteLine($"Application pool '{appPoolName}' is already running.");
-        }
+            if (site != null && site.State == ObjectState.Stopped)
+            {
+                site.Start();
+                Console.WriteLine($"Website '{siteName}' has been started.");
+            }
+            else
+            {
+                Console.WriteLine($"Website '{siteName}' is already running.");
+            }
 
-        Console.ReadLine();
-    }
-
-    static bool IsSiteStopped(string siteName)
-    {
-        using (System.DirectoryServices.DirectoryEntry site = new($"IIS://localhost/W3SVC/{siteName}"))
-        {
-            int siteState = (int)site.Properties["ServerState"].Value;
-            return siteState != 2; // 2 corresponds to "ServerStateStopped"
-        }
-    }
-
-    static void StartSite(string siteName)
-    {
-        using (System.DirectoryServices.DirectoryEntry site = new($"IIS://localhost/W3SVC/{siteName}"))
-        {
-            _ = site.Invoke("Start", null);
-            site.CommitChanges();
-        }
-    }
-
-    static bool IsAppPoolStopped(string appPoolName)
-    {
-        using (System.DirectoryServices.DirectoryEntry appPool = new($"IIS://localhost/W3SVC/AppPools/{appPoolName}"))
-        {
-            int appPoolState = (int)appPool.Properties["AppPoolState"].Value;
-            return appPoolState != 2; // 2 corresponds to "AppPoolStateStopped"
-        }
-    }
-
-    static void StartAppPool(string appPoolName)
-    {
-        using (System.DirectoryServices.DirectoryEntry appPool = new($"IIS://localhost/W3SVC/AppPools/{appPoolName}"))
-        {
-            appPool.Invoke("Start", null);
-            appPool.CommitChanges();
+            if (appPool != null && appPool.State == ObjectState.Stopped)
+            {
+                appPool.Start();
+                Console.WriteLine($"Application pool '{appPoolName}' has been started.");
+            }
+            else
+            {
+                Console.WriteLine($"Application pool '{appPoolName}' is already running.");
+            }
         }
     }
 }
