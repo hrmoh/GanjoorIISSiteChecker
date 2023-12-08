36d7bf10617c0c73c77dcd52d87ba79165a612c5
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2023-12-08 22:03:26 +0330

    working version

--- GanjoorIISSiteChecker/Program.cs
+++ GanjoorIISSiteChecker/Program.cs
@@ -4,33 +4,36 @@ class Program
 {
     static void Main()
     {
-        string siteName = "api.ganjoor.net";
-        string appPoolName = "api.ganjoor.net";
-
         using (ServerManager serverManager = new ServerManager())
         {
-            Site site = serverManager.Sites[siteName];
-            ApplicationPool appPool = serverManager.ApplicationPools[appPoolName];
+            CheckSites(serverManager, "api.ganjoor.net", "api.ganjoor.net");
+            CheckSites(serverManager, "ganjoor.net", "ganjoor.net");
+        }
+    }
 
-            if (site != null && site.State == ObjectState.Stopped)
-            {
-                site.Start();
-                Console.WriteLine($"Website '{siteName}' has been started.");
-            }
-            else
-            {
-                Console.WriteLine($"Website '{siteName}' is already running.");
-            }
+    static void CheckSites(ServerManager serverManager, string siteName, string appPoolName)
+    {
+        Site site = serverManager.Sites[siteName];
+        ApplicationPool appPool = serverManager.ApplicationPools[appPoolName];
 
-            if (appPool != null && appPool.State == ObjectState.Stopped)
-            {
-                appPool.Start();
-                Console.WriteLine($"Application pool '{appPoolName}' has been started.");
-            }
-            else
-            {
-                Console.WriteLine($"Application pool '{appPoolName}' is already running.");
-            }
+        if (site != null && site.State == ObjectState.Stopped)
+        {
+            site.Start();
+            Console.WriteLine($"Website '{siteName}' has been started.");
+        }
+        else
+        {
+            Console.WriteLine($"Website '{siteName}' is already running.");
+        }
+
+        if (appPool != null && appPool.State == ObjectState.Stopped)
+        {
+            appPool.Start();
+            Console.WriteLine($"Application pool '{appPoolName}' has been started.");
+        }
+        else
+        {
+            Console.WriteLine($"Application pool '{appPoolName}' is already running.");
         }
     }
 }
